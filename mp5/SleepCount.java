d8e001054e1c87f4a635a77618477801c3c6e021
Tyler Thames <thamestyler4@gmail.com>
2022-05-01 16:35:09 -0500

    mp5 upload

---
+++ mp5/SleepCount.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class SleepCount {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+        String tweet = value.toString();
+        String[] tweet_lines = tweet.split("\n");
+        String[] time_line = tweet_lines[0].split("\\s+");
+        if(time_line.length > 2){
+            
+            word.set(time_line[2].split(":")[0]);
+            String[] content_line = tweet_lines[2].split("\\s+");
+            for(int i = 0; i < content_line.length; i++){
+                if(content_line[i].equals("sleep") || content_line[i].equals("Sleep")){
+                    context.write(word, one);
+                    break;
+                }
+            }
+            
+        }
+
+      
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.set("textinputformat.record.delimiter", "\n\n");
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(SleepCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
